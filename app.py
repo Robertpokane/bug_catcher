c435bed17fbad46258476fbe8c7d1c8bc4e3e049
Ubuntu <ubuntu@ip-172-31-73-139.ec2.internal>
2020-06-08 18:29:12 +0000

    added base.html and bugs.html. Got bugCatcher.bugs displaying with jinja block in bugs.html in a popout accordian.

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
-app=Flask(__name__)
+
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'bug_catcher'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_bugs')
+def get_bugs():
+    return render_template("bugs.html", bugs=mongo.db.bugs.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
