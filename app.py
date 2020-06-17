61741ab2fb11ab6d5c03a302868cd6307257b11a
Ubuntu <ubuntu@ip-172-31-73-139.ec2.internal>
2020-06-17 19:45:21 +0000

    added complete, edit and delete buttons to menu. fixed bug where projects dropdown would not display at all. added completed bug list, form and wired up buttons and background funactionality

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_bugs')
 def get_bugs():
-    return render_template('bugs.html', bugs=mongo.db.bugs.find())
+    return render_template('bugs.html', bugs=mongo.db.bugs.find({'completed':{ '$ne':'on'}}))
     
 @app.route('/addbug')
 def add_bug():
@@ -34,6 +34,13 @@ def insertbug():
     bugs.insert_one(request.form.to_dict())
     return redirect(url_for('get_bugs'))
     
+@app.route('/insertproject', methods=["POST"])
+def insertproject():
+    project=mongo.db.project
+    project.insert_one(request.form.to_dict())
+    return redirect(url_for('get_projects'))    
+    
+    
 @app.route('/edit_bug/<bug_id>')
 def edit_bug(bug_id):
     the_bug =  mongo.db.bugs.find_one({"_id": ObjectId(bug_id)})
@@ -45,7 +52,68 @@ def editproject(project_id):
     project =  mongo.db.project.find_one({"_id": ObjectId(project_id)})
     return render_template('editproject.html', project=project)
     
+@app.route('/update_bug/<bug_id>', methods=["POST"])
+def update_bug(bug_id):
+    bugs = mongo.db.bugs
+    bugs.update( {'_id': ObjectId(bug_id)},
+    {
+    'project_name':request.form.get('name'),
+    'whats_wrong':request.form.get('whats_wrong'),
+    'how_to_repeat': request.form.get('how_to_repeat'),
+    'urgent':request.form.get('urgent'),
+    'reoccuring':request.form.get('reoccuring')
+    })
     
+    
+    return redirect(url_for('get_bugs')) 
+
+@app.route('/delete_bug/<bug_id>')
+def delete_bug(bug_id):
+    mongo.db.bugs.remove({"_id": ObjectId(bug_id)})
+    return redirect(url_for('get_bugs'))
+
+@app.route('/delete_project/<project_id>')
+def delete_project(project_id):
+    mongo.db.project.remove({"_id": ObjectId(project_id)})
+    return redirect(url_for('get_projects'))
+
+
+@app.route('/update_project/<project_id>', methods=["POST"]) 
+def update_project(project_id):
+    project = mongo.db.project
+    project.update({'_Id': ObjectId(project_id)},
+    {
+      'project_name':request.form.get('name'),
+      'description':request.form.get('description'),
+      'com_date':request.form.get('com_date')
+    })
+    return redirect(url_for('get_projects'))
+    
+@app.route('/complete_bug/<bug_id>')
+def complete_bug(bug_id):
+   the_bug =  mongo.db.bugs.find_one({"_id": ObjectId(bug_id)})
+   projects =  mongo.db.project.find()
+   return render_template('completebug.html', bug=the_bug, projects=projects)
+   
+@app.route('/migrate_bug/<bug_id>', methods=["POST"])
+def migrate_bug(bug_id):
+    bugs = mongo.db.bugs
+    bugs.update( {'_id': ObjectId(bug_id)},
+    {
+    'project_name':request.form.get('name'),
+    'whats_wrong':request.form.get('whats_wrong'),
+    'how_to_repeat': request.form.get('how_to_repeat'),
+    'urgent':request.form.get('urgent'),
+    'reoccuring':request.form.get('reoccuring'),
+    'fix':request.form.get('fix'),
+    'completed':request.form.get('completed')
+    })
+    return redirect(url_for('get_bugs'))
+
+@app.route('/get_complete')
+def get_complete():
+    return render_template('completedlist.html', bugs=mongo.db.bugs.find({'completed':'on'}))
+   
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
