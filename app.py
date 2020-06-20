d1d39fdd83db037ac30989402eb09fb46e16190e
Ubuntu <ubuntu@ip-172-31-73-139.ec2.internal>
2020-06-20 20:45:21 +0000

    js spelling amendment

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
+#----------------Issues code------------------------------#
 @app.route('/')
 @app.route('/get_bugs')
 def get_bugs():
@@ -20,13 +21,11 @@ def get_bugs():
 def add_bug():
     return render_template('addbugs.html', projects=mongo.db.project.find({'completed':{ '$ne':'on'}}))
     
-@app.route('/addproject')
-def add_project():
-    return render_template('new_project.html')
-    
-@app.route('/projects')
-def get_projects():
-    return render_template('projects.html', projects=mongo.db.project.find({'completed':{ '$ne':'on'}}))
+@app.route('/edit_bug/<bug_id>')
+def edit_bug(bug_id):
+    the_bug =  mongo.db.bugs.find_one({"_id": ObjectId(bug_id)})
+    projects =  mongo.db.project.find({'completed':{ '$ne':'on'}})
+    return render_template('editbug.html', bug=the_bug, projects=projects)    
     
 @app.route('/insertbug', methods=["POST"])
 def insertbug():
@@ -38,19 +37,7 @@ def insertbug():
 def insertproject():
     project=mongo.db.project
     project.insert_one(request.form.to_dict())
-    return redirect(url_for('get_projects'))    
-    
-    
-@app.route('/edit_bug/<bug_id>')
-def edit_bug(bug_id):
-    the_bug =  mongo.db.bugs.find_one({"_id": ObjectId(bug_id)})
-    projects =  mongo.db.project.find({'completed':{ '$ne':'on'}})
-    return render_template('editbug.html', bug=the_bug, projects=projects)
-    
-@app.route('/editproject/<project_id>')
-def editproject(project_id):
-    project =  mongo.db.project.find_one({"_id": ObjectId(project_id)})
-    return render_template('editproject.html', project=project)
+    return redirect(url_for('get_projects'))     
     
 @app.route('/update_bug/<bug_id>', methods=["POST"])
 def update_bug(bug_id):
@@ -65,7 +52,6 @@ def update_bug(bug_id):
     
     })
     
-    
     return redirect(url_for('get_bugs')) 
 
 @app.route('/delete_bug/<bug_id>')
@@ -76,23 +62,7 @@ def delete_bug(bug_id):
 @app.route('/remove_bug/<bug_id>')
 def remove_bug(bug_id):
     mongo.db.bugs.remove({"_id": ObjectId(bug_id)})
-    return redirect(url_for('get_complete'))
-
-@app.route('/delete_project/<project_id>')
-def delete_project(project_id):
-    mongo.db.project.remove({"_id": ObjectId(project_id)})
-    return redirect(url_for('get_projects'))
-
-
-@app.route('/update_project/<project_id>', methods=["POST"]) 
-def update_project(project_id):
-    project = mongo.db.project
-    project.update({'_Id': ObjectId(project_id)},
-    {
-      'project_name':request.form.get('name'),
-      'description':request.form.get('description')
-    })
-    return redirect(url_for('get_projects'))
+    return redirect(url_for('get_complete'))    
     
 @app.route('/complete_bug/<bug_id>')
 def complete_bug(bug_id):
@@ -113,7 +83,43 @@ def migrate_bug(bug_id):
     'fix':request.form.get('fix'),
     'completed':request.form.get('completed')
     })
-    return redirect(url_for('get_complete', bug=bugs))
+    return redirect(url_for('get_complete', bug=bugs))  
+
+#----------------Projects code------------------------------#
+
+@app.route('/addproject')
+def add_project():
+    return render_template('new_project.html')
+    
+@app.route('/projects')
+def get_projects():
+    return render_template('projects.html', projects=mongo.db.project.find({'completed':{ '$ne':'on'}}))
+
+@app.route('/editproject/<project_id>')
+def editproject(project_id):
+    project =  mongo.db.project.find_one({"_id": ObjectId(project_id)})
+    return render_template('editproject.html', project=project)
+
+@app.route('/delete_project/<project_id>')
+def delete_project(project_id):
+    mongo.db.project.remove({"_id": ObjectId(project_id)})
+    return redirect(url_for('get_projects'))
+
+
+@app.route('/update_project/<project_id>', methods=["POST"]) 
+def update_project(project_id):
+   
+    project = mongo.db.project
+    project.update( {'_id': ObjectId(project_id)},
+    {
+      'name':request.form.get('name'),
+      'description':request.form.get('description'),
+      'com_date':request.form.get('com_date')
+      
+    })
+    print(request.form)
+    return redirect(url_for('get_projects'))
+
     
 @app.route('/complete_project/<project_id>')
 def complete_project(project_id):
@@ -126,6 +132,7 @@ def get_complete():
 
 @app.route('/end_project/<project_id>', methods=["POST"])
 def end_project(project_id):
+    
     project = mongo.db.project
     project.update( {'_id': ObjectId(project_id)},
     {
@@ -136,10 +143,11 @@ def end_project(project_id):
    'com_date_actual':request.form.get('com_date_actual'),
    'delay-reason':request.form.get('delay')
     })
+    
     return redirect(url_for('get_projects', projects=project))    
 
    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
-    debug=False)
+    debug=True)
